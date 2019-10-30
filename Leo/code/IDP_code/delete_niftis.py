a300e269484640e5fc5f6bb2c0d19d3b75ada38e
olikugel <olikugel@web.de>
2019-10-30 12:31:58 +0100

    delete folders that have been moved

--- Leo/code/IDP_code/delete_niftis.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-file = sys.argv[1]
-with open(file) as f:
-    for line in f:
-        os.remove(line.rstrip('\n'))
--- Leo/code/helperfunctions/read_roi/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._read_roi import read_roi_file, read_roi_zip
-
-__all__ = ('read_roi_file', 'read_roi_zip')
