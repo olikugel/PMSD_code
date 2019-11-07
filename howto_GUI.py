42b75f5799df048251f2fc95c2b8761494145ff8
olikugel <olikugel@web.de>
2019-11-07 15:02:33 +0100

    Got started with GUI. Display images, click through.

--- howto_GUI.py
+++ howto_GUI.py
@@ -1,94 +1,123 @@
+CODEPATH = '/home/olikugel/PMSD_code'
+DATAPATH = '/home/olikugel/PMSD_data'
 import sys
-ROOTP = sys.path[-1].replace('.ipython','').replace('\\','/')
-sys.path.insert(0, ROOTP + 'Documents/Synced/_Promotion/scripts/helperfunctions')
+sys.path.insert(0, CODEPATH + '/helperfunctions')
 
-import plotting
-from plotting import print_dict as pd
 import filehandling
 import dataconversions
-import blobanalysis
-import p_leo
 
-import numpy as np
-import copy
+import matplotlib
+matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Button, RadioButtons, CheckButtons, Slider
-import matplotlib.patches as patches
-import time
+from matplotlib.widgets import Button
+import matplotlib.image as mpimg
 
-BASEP = ROOTP + 'Documents/Synced/_Promotion/Projects/Leo/'
-
-#%% 
-#
-# This script iterates through all predicted blobs for manual review. 
-# Please refer to the PDF manual for detailed instructions. In short,
-# thsi script let's you quickly click through all detected blobs and
-# decide whether they are real metastases or false positives.
-#
-# === Short instructions ============================================
-#
-# Step 1: Insert name of mouse in line 47
-# Step 2: Insert your name in line 48
-# Step 3: Press F5 and wait until loaded
-#
-# Important: Click on the "Save changes" button before closing
-#
-# ===================================================================
-# 
-# This script is based on "Refinement_2_ReviewFalsePositives".
-# Run this script after postprocessing of prediction.
-
+import traceback
+import warnings
+from functools import wraps
+def ignore_warnings(f):
+    @wraps(f)
+    def inner(*args, **kwargs):
+        with warnings.catch_warnings(record=True):
+            warnings.simplefilter("ignore")
+            response = f(*args, **kwargs)
+        return response
+    return inner
 
 #%% Main parameters 
-# Already finished..: F15, F14, H2030IC10dn573, IC14dn1, IC6dn2, IC2dn2, PC14dn04595, MCF7IC21dn528, Con1, Con2, IC6dn1, 
-# Still to do.......: 
-# Not needed for now: MCF7IC10dn529, H2030IC31dn520, IC16dn1
-# Not yet available.: Con3, Con4, Con5
-dataset           = 'Name of mouse'
+
 NAME_OF_ANNOTATOR = 'Oliver Kugel'
 
-# accumulation: "Add & next" but mark as "problematic"
-# dim metastasis: "Add & next" but mark as "dim"
-# food debris/pgments at glands/etc: "Remove & next"
-
+mice = ['H2030IC10dn573','IC2dn2','IC6dn1','IC6dn2', 'IC14dn1', 'MCF7IC21dn528']
+mouse = mice[0]
 
 #%% Set up
-
-# Prepare data & constants
-list_of_candidates = [1,2,3,4,5,6,7]
 
 # Global dynamic variables
 main_fig = plt.figure(num=101)
 main_ax_plt = main_fig.gca()
-candidate_ID = None
 
+prediction = filehandling.pload(DATAPATH + '/mice_metadata/' + mouse + '/reviewed_prediction.pickledump')
+metastases = prediction['metastases']
+TP_candidates = dataconversions.filter_dicts(metastases,'evaluation-manually_confirmed',True)
+number_of_candidates = len(TP_candidates)
+
+candidate_IDs = []
+for TP_candidate in TP_candidates:
+    candidate_IDs.append(TP_candidate['global_id'])
+    
+print('Candidate IDs: ', candidate_IDs)
 
 #%% functions
+    
+def get_filename(candidate_ID, axis):
+    
+    current_metastasis = dataconversions.filter_dicts(TP_candidates,'global_id',candidate_ID)[0]
+    patch_ID = current_metastasis['patch_id']
+    met_ID = current_metastasis['id']
+    patch_ID_padded = filehandling.pad_ID(patch_ID)
+    met_ID_padded = filehandling.pad_ID(met_ID)
+    
+    filename_prefix = 'patch' + patch_ID_padded + '_met' + met_ID_padded
+    filename = filename_prefix + '_' + axis + '.png'
+    
+    return filename
+    
+
+@ignore_warnings
 def update_plot():
     global main_fig
 
     plt.figure(num=main_fig.number)
+
+    # Load all 3x3 projection-images for given candidate ID
     
-    # Load all 3x3 projections for given candidate ID
-    p_C00_y = np.random.rand(50,50) #filehandling.pload(PATH + 'canidate_' + str(candidate_ID) + '_C00_z')
-    p_C00_x = np.zeros((50,50))
-    p_C00_z = np.zeros((50,50))
+    # channel C00
+    filepath = DATAPATH + '/Potential_TP_Metastases/' + mouse + '/C00/ROI_50/PNG/'
+    image_C00_y = mpimg.imread(filepath + get_filename(candidate_ID, 'y'))
+    image_C00_x = mpimg.imread(filepath + get_filename(candidate_ID, 'x'))
+    image_C00_z = mpimg.imread(filepath + get_filename(candidate_ID, 'z'))
+
+    # channel C01
+    filepath = DATAPATH + '/Potential_TP_Metastases/' + mouse + '/C01/ROI_50/PNG/'
+    image_C01_y = mpimg.imread(filepath + get_filename(candidate_ID, 'y'))
+    image_C01_x = mpimg.imread(filepath + get_filename(candidate_ID, 'x'))
+    image_C01_z = mpimg.imread(filepath + get_filename(candidate_ID, 'z'))
     
-    p_C01_y = np.zeros((50,50))
-    p_C01_x = np.zeros((50,50))
-    p_C01_z = np.zeros((50,50))
-    
-    p_C02_y = np.zeros((50,50))
-    p_C02_x = np.zeros((50,50))
-    p_C02_z = np.zeros((50,50))
+    # channel C02
+    filepath = DATAPATH + '/Potential_TP_Metastases/' + mouse + '/C02/ROI_50/PNG/'
+    image_C02_y = mpimg.imread(filepath + get_filename(candidate_ID, 'y'))
+    image_C02_x = mpimg.imread(filepath + get_filename(candidate_ID, 'x'))
+    image_C02_z = mpimg.imread(filepath + get_filename(candidate_ID, 'z'))
     
     # Update subplots
-    ax = plt.subplot(3,3,1)
+    
+    # channel C00
+    ax = plt.subplot(3,2,1)
     ax.cla()
-    plt.imshow(p_C00_y)
+    plt.imshow(image_C00_z) # z
+    ax = plt.subplot(3,2,2)
+    ax.cla()
+    plt.imshow(image_C00_x) # x
+    
+    # channel C01
+    ax = plt.subplot(3,2,3)
+    ax.cla()
+    plt.imshow(image_C01_z) # z
+    ax = plt.subplot(3,2,4)
+    ax.cla()
+    plt.imshow(image_C01_x) # x
+    
+    # channel C02
+    ax = plt.subplot(3,2,5)
+    ax.cla()
+    plt.imshow(image_C02_z) # z
+    ax = plt.subplot(3,2,6)
+    ax.cla()
+    plt.imshow(image_C02_x) # x
     
     # Update rest
-    plt.suptitle(dataset + '\nCanidate 13 of 128')
+    plt.suptitle('Mouse ' + mouse + '\nCandidate ' + str(candidate_ID) + ' of ' + str(number_of_candidates))
     plt.draw()
     plt.subplots_adjust(top   = 0.85)
     plt.subplots_adjust(right = 0.98)
@@ -97,21 +126,26 @@ def update_plot():
 
 def next_candidate(not_needed=False):
     # define optional argument so that function can be called from button
-    global list_of_candidates, candidate_ID, candidate_dict
+    global candidate_IDs, candidate_ID, candidate_dict
     try:
-        candidate_ID = list_of_candidates.pop(0)
+        candidate_ID = candidate_IDs.pop(0)
+        print()
+        print('Current candidate: ', candidate_ID)
+        print(candidate_IDs)
         # load candidate_dict
-        candidate_dict = {}
-        candidate_dict['evaluation'] = {}
-        candidate_dict['evaluation']['XYAZ'] = None
+        # candidate_dict = {}
+        # candidate_dict['evaluation'] = {}
+        # candidate_dict['evaluation']['XYAZ'] = None
         update_plot()
-    except:
-        print("\n\nNo more candidates to review.")
+    except Exception as e:
+        print(e)
+        # traceback.print_exc()
+        # print("\n\nNo more candidates to review.")
 
 
 def mark_as_FP(event):
     global candidate_dict
-    candidate_dict['evaluation']['XYAZ'] = False
+    # candidate_dict['evaluation']['XYAZ'] = False
     save_candidate()
     print("Candidate was marked as FP")
     next_candidate()
@@ -119,7 +153,7 @@ def mark_as_FP(event):
 
 def mark_as_TP(event):
     global candidate_dict
-    candidate_dict['evaluation']['XYAZ'] = True
+    # candidate_dict['evaluation']['XYAZ'] = True
     save_candidate()
     print("Candidate was marked as TP")
     next_candidate()
@@ -127,7 +161,7 @@ def mark_as_TP(event):
 
 def mark_as_unclear(event):
     global candidate_dict
-    candidate_dict['evaluation']['XYAZ'] = None
+    # candidate_dict['evaluation']['XYAZ'] = None
     save_candidate()
     print("Candidate was marked as unclear")
     next_candidate()
@@ -148,12 +182,9 @@ def save_to_file(event):
 
 # window: Create
 plt.figure(main_fig.number)
-plt.get_current_fig_manager().window.showMaximized()
-plt.draw()
-plt.pause(1)
-plt.draw()
-screensize = main_fig.get_size_inches()*main_fig.dpi
 
+plt.get_current_fig_manager().window.showMaximized()
+screensize = main_fig.get_size_inches()*main_fig.dpi
 
 # window: Save
 main_wp_s = [0.03, 0.9, 0.10, 0.08]
@@ -167,8 +198,6 @@ main_wa_t = plt.axes(main_wp_t)
 main_w_t = Button(main_wa_t, 'Mark as TP')
 main_w_t.on_clicked(mark_as_TP) 
 
-#%% Main
-
 next_candidate()
-
+plt.show(block=True)
 
