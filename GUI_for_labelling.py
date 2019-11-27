dc103a7a64db9d8dd0c649fc8f49d47e7b2f223f
olikugel <olikugel@web.de>
2019-11-27 18:17:33 +0100

    replaced mac address with machine_ID

--- GUI_for_labelling.py
+++ GUI_for_labelling.py
@@ -1,14 +1,26 @@
-from uuid import getnode as get_mac
-mac_address = get_mac()
-mac_address = str(hex(mac_address)).replace('0x','')
-if mac_address == 'd16adaee4245': # running on remote server
+import sys
+import subprocess
+
+bashCommand = "hostnamectl"
+process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
+output = str(output)
+output = output.replace("b'", "").replace("'", "")
+lines = output.split('\\n')
+for line in lines:
+    if "Machine ID" in line:
+        machine_ID = line.strip().replace("Machine ID: ", "")
+        print("\nMachine ID: ", machine_ID, "\n")
+
+if machine_ID == '83011c94751c4629a5d4c426051b3041':   # running on local machine
+  CODEPATH = '/home/olikugel/PMSD_code'
+  DATAPATH = '/home/olikugel/PMSD_data'  
+elif machine_ID == '14bef0a2877e464f877ca6ab665b81e8': # running on remote server
   CODEPATH = '/home/oschoppe/Documents/OKugel/PMSD_code'
   DATAPATH = '/home/oschoppe/Documents/OKugel/PMSD_data'  
-else: # running on local machine
-  CODEPATH = '/home/olikugel/PMSD_code'
-  DATAPATH = '/home/olikugel/PMSD_data'    
+else:
+    sys.exit('Running on unknown machine. Cannot set basepaths. Exiting.')
 
-import sys
 sys.path.insert(0, CODEPATH + '/helperfunctions')
 
 import filehandling
